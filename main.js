71d701e0a7046af02afca67731ddc16eedfe0527
Dankore <zimmazone@yahoo.com>
2019-06-03 16:38:25 +0000

    end tut

---
+++ main.js
@@ -0,0 +1,58 @@
+ const addItems = document.querySelector('.add-items');
+  const itemsList = document.querySelector('.plates');
+  const items = JSON.parse(localStorage.getItem('items')) || [];
+  
+  // Add items
+  addItems.addEventListener('submit', addItem);
+  
+  function addItem(e) {
+   // Prevent refresh after every form submission
+   e.preventDefault();
+   
+   const text = (this.querySelector('[name=item]')).value
+   const item = {
+    text: text, // Or just text (ES6)
+    done: false
+   }
+   
+   // Push the submitted items to the items array
+   items.push(item);
+   populateList(items, itemsList);
+   localStorage.setItem('items', JSON.stringify(items));
+   this.reset();
+  }
+  
+  function populateList(plates = [], plateList) {
+   plateList.innerHTML = plates.map((plate, i) => {
+    return `
+     <li>
+     <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+      <label for="item${i}">${plate.text}</label>
+     </li>
+    `
+   }).join('');
+ 
+  }
+  
+  populateList(items, itemsList);
+  itemsList.addEventListener('click', toggleDone);
+  
+  function toggleDone(e) {
+   if(!e.target.matches('input')) return // Skip unless its input
+   const el = e.target;
+   const index = el.dataset.index;
+   items[index].done = !items[index].done;
+   localStorage.setItem('items', JSON.stringify(items));
+   populateList(items, itemsList);
+  }
+  
+// Customization
+
+//Clear all
+const button1 = document.querySelector('.button1');
+button1.addEventListener('click', clearAll);
+
+function clearAll(e) {
+
+console.log( itemsList.innerHTML);
+}
